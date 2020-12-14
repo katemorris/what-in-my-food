0466fd33209bb69df28b63c30bf8ecff0327651d
Kate Tester <katemorris@gmail.com>
2020-12-14 10:13:54 -0700

    Adds tests for food PORO

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -2,5 +2,6 @@ class FoodsController < ApplicationController
   def index
     @ingredient = params[:q]
     @foods = FoodFacade.make_food(@ingredient)
+    @total_results = FoodFacade.results_count(@ingredient)
   end
 end
---
+++ spec/poros/food_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+
+RSpec.describe Food do
+  let(:food) do
+    Food.new({
+      :fdcId=>1119530,
+      :description=>"SWEETS MEDLEY SEL GRIS, BATATA, SWEET POTATO, PURPLE SWEET POTATO REAL VEGETABLE CHIPS, SWEETS MEDLEY",
+      :lowercaseDescription=>"sweets medley sel gris, batata, sweet potato, purple sweet potato real vegetable chips, sweets medley",
+      :dataType=>"Branded",
+      :gtinUpc=>"728229015529",
+      :publishedDate=>"2020-11-13",
+      :brandOwner=>"The Hain Celestial Group, Inc.",
+      :ingredients=>"A BLEND OF SWEET POTATOES (SWEET POTATO, PURPLE SWEET POTATO, BATATA), AVOCADO OIL, SEA SALT.",
+    }
+    )
+  end
+
+  describe 'attribute accessors' do
+    it 'has attributes' do
+      description = "SWEETS MEDLEY SEL GRIS, BATATA, SWEET POTATO, PURPLE SWEET POTATO REAL VEGETABLE CHIPS, SWEETS MEDLEY"
+      ingredients = "A BLEND OF SWEET POTATOES (SWEET POTATO, PURPLE SWEET POTATO, BATATA), AVOCADO OIL, SEA SALT."
+      expect(food.id).to eq(1119530)
+      expect(food.upc).to eq('728229015529')
+      expect(food.brand).to eq('The Hain Celestial Group, Inc.')
+      expect(food.description).to eq(description)
+      expect(food.ingredients).to eq(ingredients)
+    end
+  end
+end
